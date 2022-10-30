0a5ef03752f5a970cf13e5140131f8632e65f2fa
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-10-29 17:59:16 -0700

    added axios get and post commands to get data from api

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,90 +1,59 @@
-//Array containing the given comments
-const commentArray = [
-  {
-    name: "Miles Acosta",
-    time: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    image: "",
-  },
-  {
-    name: "Emilie Beach",
-    time: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    image: "",
-  },
-  {
-    name: "Connor Walton",
-    time: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    image: "",
-  },
-];
+const key = "6f941b6e-29ae-4076-bbb9-46d5cfde15b5";
 
-//Function to render comments contained in array
-
-const displayComments = (ob) => {
+const displayComments = () => {
   const selectComments = document.querySelector(".comments__container");
   selectComments.innerHTML = "";
-  let objLength = Object.keys(ob);
-  if (objLength.length != 0) {
-    commentArray.push(ob);
-  }
-  for (let i = commentArray.length - 1; i >= 0; i--) {
-    const fullName = document.createElement("h3");
-    fullName.classList.add("comments__full");
-    fullName.innerText = commentArray[i].name;
+  axios
+    .get(`https://project-1-api.herokuapp.com/comments?api_key=${key}`)
+    .then((response) => {
+      const commentArray = response.data;
+      commentArray.sort((a, b) => b.timestamp - a.timestamp);
 
-    const time = document.createElement("p");
-    time.classList.add("comments__time");
-    time.innerText = commentArray[i].time;
+      for (let i = 0; i < commentArray.length; i++) {
+        const fullName = document.createElement("h3");
+        fullName.classList.add("comments__full");
+        fullName.innerText = commentArray[i].name;
 
-    const comm = document.createElement("p");
-    comm.classList.add("comments__commenttext");
-    comm.innerText = commentArray[i].comment;
+        const time = document.createElement("p");
+        time.classList.add("comments__time");
+        time.innerText = getTimeFull(commentArray[i].timestamp);
 
-    const commentsLists = document.createElement("div");
-    commentsLists.classList.add("comments__lists");
+        const comm = document.createElement("p");
+        comm.classList.add("comments__commenttext");
+        comm.innerText = commentArray[i].comment;
 
-    if (commentArray[i].image == "") {
-      const imageContainer = document.createElement("div");
-      imageContainer.classList.add("comments__profile-pic");
-      imageContainer.innerText = "";
-      commentsLists.appendChild(imageContainer);
-    } else {
-      const imageContainer = document.createElement("img");
-      imageContainer.classList.add("comments__profile-pic");
-      imageContainer.setAttribute("src", commentArray[i].image);
-      commentsLists.appendChild(imageContainer);
-    }
+        const commentsLists = document.createElement("div");
+        commentsLists.classList.add("comments__lists");
 
-    const fullNameTime = document.createElement("div");
-    fullNameTime.classList.add("comments__fullname-time");
+        if (commentArray[i].image == undefined) {
+          const imageContainer = document.createElement("div");
+          imageContainer.classList.add("comments__profile-pic");
+          imageContainer.innerText = "";
+          commentsLists.appendChild(imageContainer);
+        } else {
+          const imageContainer = document.createElement("img");
+          imageContainer.classList.add("comments__profile-pic");
+          imageContainer.setAttribute("src", commentArray[i].image);
+          commentsLists.appendChild(imageContainer);
+        }
 
-    const upper = document.createElement("div");
-    upper.classList.add("comments__upper");
+        const fullNameTime = document.createElement("div");
+        fullNameTime.classList.add("comments__fullname-time");
 
-    selectComments.appendChild(commentsLists);
+        const upper = document.createElement("div");
+        upper.classList.add("comments__upper");
 
-    commentsLists.appendChild(fullNameTime);
-    fullNameTime.appendChild(upper);
-    fullNameTime.appendChild(comm);
-    upper.appendChild(fullName);
-    upper.appendChild(time);
-  }
+        selectComments.appendChild(commentsLists);
+
+        commentsLists.appendChild(fullNameTime);
+        fullNameTime.appendChild(upper);
+        fullNameTime.appendChild(comm);
+        upper.appendChild(fullName);
+        upper.appendChild(time);
+      }
+    });
 };
 
-//Function to get time
-
-const getTimeFull = () => {
-  const d = new Date();
-  const year = d.getFullYear();
-  const month = d.getMonth() + 1;
-  const date = d.getDate();
-  return `${month > 9 ? "" : 0}${month}/${date > 9 ? "" : 0}${date}/${year}`;
-};
 let nameInput = document.querySelector(".comments__name");
 let commInput = document.querySelector(".comments__comment");
 
@@ -99,10 +68,18 @@ const checkingContent = (e) => {
     commInput.classList.add("comments__comment--error");
   }
 };
-const obj = {};
+
+const getTimeFull = (d) => {
+  const da = new Date(d);
+  const year = da.getFullYear();
+  const month = da.getMonth() + 1;
+  const date = da.getDate();
+  return `${month > 9 ? "" : 0}${month}/${date > 9 ? "" : 0}${date}/${year}`;
+};
+
 //Load comments when Dom loaded
 document.addEventListener("DOMContentLoaded", () => {
-  displayComments(obj);
+  displayComments();
   const formInput = document.querySelector(".comments__form");
 
   //Event listener for the form
@@ -111,17 +88,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     checkingContent(event);
     if (event.target.name.value != "" && event.target.comment.value != "") {
-      let obj1 = {
-        name: event.target.name.value,
-        time: getTimeFull(),
-        comment: event.target.comment.value,
-        image: "./assets/images/Mohan-muruge.jpg",
-      };
+      axios
+        .post(`https://project-1-api.herokuapp.com/comments?api_key=${key}`, {
+          name: event.target.name.value,
+          comment: event.target.comment.value,
+        })
+        .then((response) => {
+          displayComments();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
       event.target.comment.value = "";
       event.target.name.value = "";
-
-      displayComments(obj1);
     }
   });
 });
@@ -135,3 +115,11 @@ commInput.addEventListener("click", (event) => {
   nameInput.classList.remove("comments__name--error");
   commInput.classList.remove("comments__comment--error");
 });
+
+// axios
+//   .delete(
+//     `https://project-1-api.herokuapp.com/comments/4b102fd7-2bb3-4a7c-a5ed-021f67fcbb5b?api_key=${key}`
+//   )
+//   .then((response) => {
+//     console.log(response);
+//   });
